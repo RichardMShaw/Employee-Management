5f81385af9cb1ba6f559d347465231f45662f2b6
Richard Shaw <richardmorganshaw@gmail.com>
2020-12-02 13:55:13 -0800

    Setting up Routes

--- routes/employeesRoutes.js
+++ routes/employeesRoutes.js
@@ -1,3 +1,8 @@
 const router = require('express').Router()
+const db = mysql.createConnection('mysql://root:rootroot@localhost/employees_db')
+
+router.post('/employees',(req,res)=>{
+
+})
 
 module.exports = router
